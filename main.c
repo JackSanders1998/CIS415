0019a41e3b7e3bc17f3e6f1a8b10c730500eba87
JackSanders1998 <41480596+JackSanders1998@users.noreply.github.com>
2020-05-08 12:36:27 -0700

    Update main.c

--- main.c
+++ main.c
@@ -15,6 +15,15 @@ void signaler(pid_t *children, int siglabel) {
   }
 }
 
+int child_alive(pid_t process) {
+    int status;
+    pid_t curr_child = waitpid(process, &status, WNOHANG);
+    if (status > 0)
+        return 1;
+    else
+        return 0;
+}
+
 int main(int argc, char **argv) {
     pid_t processes[5];
     pid_t curproc;
@@ -47,43 +56,47 @@ int main(int argc, char **argv) {
     sleep(3);
     signaler(processes, SIGUSR1);
     //pid_t curr_child, next_child;
-    int status_curr, status_next;
+    //int status_curr, status_next;
+    //int alive
     //processes[5] = -1;
+    int i = 0;
+    while (child_alive(processes[i]) && child_alive(processes[i+1])) {
+        kill(processes[i], SIGSTOP);
+        kill(processes[i+1], SIGCONT);
+            //curr_child = waitpid(processes[i], &status_curr, WNOHANG);
+            //next_child = waitpid(processes[i+1], &status_next, WNOHANG);
+            //printf("curr_child: %d\n", curr_child);
+            //printf("next_child: %d\n", next_child);
 
-    for (int i = 0; i < 5; i++) {
-    //int i = 0;
-    //while (processes[i] != -1) {
-        //curr_child = waitpid(processes[i], &status_curr, WNOHANG);
-        //next_child = waitpid(processes[i+1], &status_next, WNOHANG);
-        //printf("curr_child: %d\n", curr_child);
-        //printf("next_child: %d\n", next_child);
+            /*
+            if (waitpid(processes[i], &status_curr, WNOHANG) >= 0) {
+                kill(processes[i], SIGSTOP);
+            }
+            if (waitpid(processes[i+1], &status_curr, WNOHANG) >= 0) {
+                kill(processes[i+1], SIGSTOP);
+            }
+            //i++;
+            */
 
-        if (waitpid(processes[i], &status_curr, WNOHANG) >= 0) {
-            kill(processes[i], SIGSTOP);
-        }
-        if (waitpid(processes[i+1], &status_curr, WNOHANG) >= 0) {
-            kill(processes[i+1], SIGSTOP);
-        }
-        //i++;
-
-        /*
-        if (kill(processes[i], SIGSTOP) > 0) {
-            printf("current(%d) child alive\n", i);
-        }
-        else {
-            // do something
-            printf("current(%d) child dead\n", i);
-        }
-        if (kill(processes[i+1], SIGCONT) > 0) {
-            printf("next(%d) child alive\n", (i+1));
-        }
-        else {
-            // do something
-            printf("next(%d) child dead\n", (i+1));
-        }
-        */
-        sleep(1);
+            /*
+            if (kill(processes[i], SIGSTOP) > 0) {
+                printf("current(%d) child alive\n", i);
+            }
+            else {
+                // do something
+                printf("current(%d) child dead\n", i);
+            }
+            if (kill(processes[i+1], SIGCONT) > 0) {
+                printf("next(%d) child alive\n", (i+1));
+            }
+            else {
+                // do something
+                printf("next(%d) child dead\n", (i+1));
+            }
+            */
+        i++;
     }
+    sleep(1);
     return 0;
 }
 
